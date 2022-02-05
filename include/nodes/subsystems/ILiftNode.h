cc52af2ed248c2ffe67b47fd857ba3080882550a
Michael Conner <connerm@msoe.edu>
2022-02-05 17:57:50 -0600

    creating ILiftNode.h

--- include/nodes/subsystems/ILiftNode.h
+++ include/nodes/subsystems/ILiftNode.h
@@ -1,1 +1,22 @@
-// TODO
+#pragma once
+
+#include "nodes/NodeManager.h"
+
+class ILiftNode : public Node {
+public:
+    ILiftNode(NodeManager* node_manager, std::string handle_name);
+
+    virtual void initialize() = 0;
+
+    virtual void setLiftVoltage(int voltage) = 0;
+
+    virtual void setLiftVelocity(int velocity) = 0;
+
+    virtual void setLiftPosition(int position) = 0;
+
+    virtual void teleopPeriodic() {};
+
+    virtual void autonPeriodic() {};
+
+    virtual ~ILiftNode();
+};
