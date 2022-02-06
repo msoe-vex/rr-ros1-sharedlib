5ee1d8411bb71172aa23d5c14cac46546a2ba4ef
Carl <malagodicarl@gmail.com>
2022-02-05 18:15:04 -0600

    Added claw interface node

--- include/nodes/subsystems/IClawNode.h
+++ include/nodes/subsystems/IClawNode.h
@@ -1,1 +1,20 @@
-// TODO
+#pragma once
+
+#include "nodes/NodeManager.h"
+
+class IClawNode : public Node {
+public:
+    IClawNode(NodeManager* node_manager, std::string handle_name);
+
+    virtual void initialize() = 0;
+
+    virtual void openClaw(bool open) = 0;
+
+    virtual void closeClaw(bool closed)= 0;
+
+    virtual void teleopPeriodic() {};
+
+    virtual void autonPeriodic() {};
+
+    virtual ~IClawNode();
+};
