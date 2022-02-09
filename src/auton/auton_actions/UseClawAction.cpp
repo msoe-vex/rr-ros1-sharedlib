e17476207cf92930e82f9800a6ea7a507e888394
Carl <malagodicarl@gmail.com>
2022-02-08 19:53:07 -0600

    Added UseClawAction

---
+++ src/auton/auton_actions/UseClawAction.cpp
@@ -0,0 +1,24 @@
+#include "auton/auton_actions/UseClawAction.h"
+
+UseClawAction::UseClawAction(IClawNode* claw_node, bool position) : 
+        m_claw_node(claw_node), 
+        m_position(position)  {
+    
+}
+
+void UseClawAction::ActionInit() {
+
+}
+
+AutonAction::actionStatus UseClawAction::Action() {
+    if(m_position) {
+        m_claw_node->openClaw();
+        return END;
+    }
+    m_claw_node-> closeClaw();
+    return END;
+}
+
+void IntakeAction::ActionEnd() {
+    
+}
