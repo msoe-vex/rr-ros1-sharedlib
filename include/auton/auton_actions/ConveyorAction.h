671e53be92c1f09f9d0bf33b2a07f1feff12586d
NathanDuPont <nathan.dupont01@gmail.com>
2022-02-10 01:17:10 -0600

    Renamed files and fixed includes

--- include/auton/auton_actions/ConveyorAction.h
+++ include/auton/auton_actions/ConveyorAction.h
@@ -4,17 +4,17 @@
 #include "lib-rr/auton/Auton.h"
 #include "lib-rr/util/Timer.h"
 #include "lib-rr/util/Constants.h"
-#include "lib-rr/nodes/subsystems/IConveyerNode.h"
+#include "lib-rr/nodes/subsystems/IConveyorNode.h"
 
-class ConveyerAction : public AutonAction {
+class ConveyorAction : public AutonAction {
 private:
-    IConveyerNode* m_conveyer_node;
+    IConveyorNode* m_conveyor_node;
     Timer m_timer;
     int m_voltage;
     double m_time; // Time in seconds
 
 public:
-    ConveyerAction(IConveyerNode* conveyer_node, int voltage=MAX_MOTOR_VOLTAGE, double time=0);
+    ConveyorAction(IConveyorNode* conveyor_node, int voltage=MAX_MOTOR_VOLTAGE, double time=0);
 
     void ActionInit();
 
