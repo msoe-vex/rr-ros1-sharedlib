c9da0513167f93e8eee8729c034f9ebe89b03b87
Carl Malagodi <malagodicarl@gmail.com>
2022-04-15 15:02:17 -0500

    attempted to fix drivestraight acction, still needs to be tuned

--- src/auton/auton_actions/DriveStraightAction.cpp
+++ src/auton/auton_actions/DriveStraightAction.cpp
@@ -1,8 +1,9 @@
 #include "lib-rr/auton/auton_actions/DriveStraightAction.h"
 
-DriveStraightAction::DriveStraightAction(IDriveNode* drive_node, double distance, double max_velocity, 
+DriveStraightAction::DriveStraightAction(IDriveNode* drive_node, OdometryNode* odometry_node, double distance, double max_velocity, 
         double max_accel) :
         m_drive_node(drive_node), 
+        m_odometry_node(odometry_node),
         m_distance(distance), // in inches
         m_max_velocity(max_velocity), 
         m_max_accel(max_accel), 
@@ -14,10 +15,20 @@ DriveStraightAction::DriveStraightAction(IDriveNode* drive_node, double distance
 void DriveStraightAction::ActionInit() {
     m_timer.Start();
     m_drive_node->resetEncoders();
+    m_starting_pose = m_odometry_node->getCurrentPose(); //used to keep track of the starting x coordinate
 }
 
 AutonAction::actionStatus DriveStraightAction::Action() {
     double dt = m_timer.Get() - m_lastTime;
+
+    //getting the error in the x direction
+    m_current_pose = m_odometry_node->getCurrentPose(); //used to keep track of the current x position
+    double x_error = (m_current_pose.position.x() - m_starting_pose.position.x());
+    //calculating the offset in motor velocity due to the x error
+    double k = 0.002; //proportional constant for calculating offset
+    double offset = k*x_error*m_max_velocity; //offset to be added and subtacted to the left and right sides velocity
+    std::cout << "current calculated offset: " << offset << std::endl;
+
     double speed = m_max_velocity;
 
     double accel = (m_max_velocity - m_lastSpeed) / dt;
@@ -39,12 +50,18 @@ AutonAction::actionStatus DriveStraightAction::Action() {
     speed = max(speed, m_feedForward);
 
     m_lastSpeed = speed;
+    //calculating left and right speeds
+    double leftSpeed = speed - offset;
+    std::cout << "leftSpeed: " << leftSpeed << std::endl;
+    double rightSpeed = speed + offset;
+    std::cout << "rightSpeed: " << rightSpeed << std::endl;
     m_lastTime = m_timer.Get();
     if (remainingDistance < 0.5) {
         return END;
     } else {
         if (m_distance > 0) {
-            m_drive_node->setDriveVelocity(speed, 0);
+            m_drive_node->setRightVelocity(rightSpeed);
+            m_drive_node->setLeftVelocity(leftSpeed);
         } else {
             m_drive_node->setDriveVelocity(-speed, 0);
         }
