71d6d2ccad9b902ad42e33894bb630e90cbb2f40
Carl Malagodi <malagodicarl@gmail.com>
2022-04-27 23:45:40 -0500

    fixed adiDigitalIn constructor

--- src/nodes/sensor_nodes/ADIDigitalInNode.cpp
+++ src/nodes/sensor_nodes/ADIDigitalInNode.cpp
@@ -3,7 +3,7 @@
 // By default, this constructor calls the constructor for the Node object in
 // NodeManager.h
 ADIDigitalInNode::ADIDigitalInNode(NodeManager* node_manager, int port,
-    std::string handle_name, bool reverse=false) : Node(node_manager, 10),
+    std::string handle_name) : Node(node_manager, 10),
      m_digital_in(port) {
     m_handle_name = handle_name.insert(0, "sensor/");
     m_sub_publish_data_name = m_handle_name + "/publish";
@@ -15,7 +15,7 @@ ADIDigitalInNode::ADIDigitalInNode(NodeManager* node_manager, int port,
 }
 
 ADIDigitalInNode::ADIDigitalInNode(NodeManager* node_manager, pros::ext_adi_port_pair_t port_pair,
-    std::string handle_name, bool reverse=false) : Node(node_manager, 10), m_digital_in(port_pair) {
+    std::string handle_name) : Node(node_manager, 10), m_digital_in(port_pair) {
     m_handle_name = handle_name.insert(0, "sensor/");
     m_sub_publish_data_name = m_handle_name + "/publish";
 
