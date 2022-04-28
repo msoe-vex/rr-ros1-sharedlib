b2b1037c91d91f26a60e1fcccf582f70b565c78e
Carl Malagodi <malagodicarl@gmail.com>
2022-04-27 20:59:42 -0500

    updates to all adi sensors for multiple sensors

--- include/nodes/sensor_nodes/ADIUltrasonicNode.h
+++ include/nodes/sensor_nodes/ADIUltrasonicNode.h
@@ -20,8 +20,9 @@ private:
     void m_publishData(const std_msgs::Empty& msg);
 
 public:
-    ADIUltrasonicNode(NodeManager* node_manager, int port_ping, int port_echo,
-        std::string handle_name);
+    ADIUltrasonicNode(NodeManager* node_manager, int port_ping, int port_echo, std::string handle_name);
+
+    ADIUltrasonicNode(NodeManager* node_manager, pros::ext_adi_port_tuple_t port_tuple, std::string handle_name);
 
     void initialize();
 
